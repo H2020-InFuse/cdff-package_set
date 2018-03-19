fb63cea0008eaa3627449fed2f6704b98cb8ba50
Steffen Planthaber <Steffen.Planthaber@dfki.de>
2018-03-19 18:43:38 +0100

    add boost

--- overrides.rb
+++ overrides.rb
@@ -1,3 +1,4 @@
+Autoproj.add_osdeps_overrides 'boost', :package => 'cdff/external/boost', :force => true
 Autoproj.add_osdeps_overrides 'opencv', :package => 'cdff/external/opencv', :force => true
 Autoproj.add_osdeps_overrides 'cmake', :package => 'cdff/external/cmake', :force => true
 Autoproj.add_osdeps_overrides 'eigen3', :package => 'cdff/external/eigen', :force => true
