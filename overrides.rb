d3bb5b04d5fa82f78bcd5bcab7d9ff5618cfc433
Steffen Planthaber <Steffen.Planthaber@dfki.de>
2018-03-21 16:06:00 +0100

    opencv override

--- overrides.rb
+++ overrides.rb
@@ -1,5 +1,5 @@
 #Autoproj.add_osdeps_overrides 'boost', :package => 'cdff/external/boost', :force => true
-#Autoproj.add_osdeps_overrides 'opencv', :package => 'cdff/external/opencv', :force => true
+Autoproj.add_osdeps_overrides 'opencv', :package => 'cdff/external/opencv', :force => true
 #Autoproj.add_osdeps_overrides 'cmake', :package => 'cdff/external/cmake', :force => true
 #Autoproj.add_osdeps_overrides 'eigen3', :package => 'cdff/external/eigen', :force => true
 #Autoproj.add_osdeps_overrides 'flann', :package => 'cdff/external/flann', :force => true
