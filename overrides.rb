ba1fecaa26245770367941b3d03691b0d1655842
Steffen Planthaber <Steffen.Planthaber@dfki.de>
2018-04-04 16:41:26 +0200

    updated source builds for required

--- overrides.rb
+++ overrides.rb
@@ -8,16 +8,15 @@ if (Autoproj.user_config('CDFF_EXTERNAL_SOURCE_INSTALL') == 'all') then
     Autoproj.add_osdeps_overrides 'qhull', :package => 'cdff/external/qhull', :force => true
     Autoproj.add_osdeps_overrides 'external/tinyxml', :package => 'cdff/external/tinyxml2', :force => true
     Autoproj.add_osdeps_overrides 'yaml-cpp', :package => 'cdff/external/yaml-cpp', :force => true
-    Autoproj.add_osdeps_overrides 'boost', :package => 'cdff/external/boost', :force => true
     Autoproj.add_osdeps_overrides 'flann', :package => 'cdff/external/flann', :force => true
     Autoproj.add_osdeps_overrides 'vtk', :package => 'cdff/external/vtk', :force => true    
+    Autoproj.add_osdeps_overrides 'opencv', :package => 'cdff/external/opencv', :force => true
 end
 
 #required packages (newer versions than Ubuntu 16.04 needed)
 if (Autoproj.user_config('CDFF_EXTERNAL_SOURCE_INSTALL') == 'all') || (Autoproj.user_config('CDFF_EXTERNAL_SOURCE_INSTALL') == 'required') then
-    Autoproj.add_osdeps_overrides 'cmake', :package => 'cdff/external/cmake', :force => true
-    Autoproj.add_osdeps_overrides 'eigen3', :package => 'cdff/external/eigen', :force => true
-    Autoproj.add_osdeps_overrides 'opencv', :package => 'cdff/external/opencv', :force => true
-    Autoproj.add_osdeps_overrides 'slam/pcl', :package => 'cdff/external/pcl', :force => true
+        Autoproj.add_osdeps_overrides 'cmake', :package => 'cdff/external/cmake', :force => true
+        Autoproj.add_osdeps_overrides 'eigen3', :package => 'cdff/external/eigen', :force => true
+        #Autoproj.add_osdeps_overrides 'slam/pcl', :package => 'cdff/external/pcl', :force => true
 end
 
