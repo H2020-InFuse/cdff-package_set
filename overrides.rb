c4ccaa463b7acd2f941937af9b2d0adfb3cefa7e
Steffen Planthaber <Steffen.Planthaber@dfki.de>
2018-05-31 11:37:13 +0200

    remove tinyxml override

--- overrides.rb
+++ overrides.rb
@@ -7,7 +7,7 @@
 if (Autoproj.user_config('CDFF_EXTERNAL_SOURCE_INSTALL') == 'all') then
 #    Autoproj.add_osdeps_overrides 'cmake', :package => 'cdff/external/cmake', :force => true
     Autoproj.add_osdeps_overrides 'qhull', :package => 'cdff/external/qhull', :force => true
-    Autoproj.add_osdeps_overrides 'external/tinyxml', :package => 'cdff/external/tinyxml2', :force => true
+#    Autoproj.add_osdeps_overrides 'external/tinyxml', :package => 'cdff/external/tinyxml2', :force => true
     Autoproj.add_osdeps_overrides 'yaml-cpp', :package => 'cdff/external/yaml-cpp', :force => true
     Autoproj.add_osdeps_overrides 'flann', :package => 'cdff/external/flann', :force => true
     Autoproj.add_osdeps_overrides 'vtk', :package => 'cdff/external/vtk', :force => true    
