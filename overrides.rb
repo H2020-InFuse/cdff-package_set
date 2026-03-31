0899ae8093d6ac605668db52131f4b135bff3c47
Raul Dominguez <Raul.Dominguez@dfki.de>
2026-03-31 16:32:31 +0200

    Use the apt version of pcl instead of the build from source

--- overrides.rb
+++ overrides.rb
@@ -18,7 +18,7 @@ end
 #required packages (newer versions than Ubuntu 16.04 needed), ubuntu 17.10 needs none
 if (Autoproj.user_config('CDFF_EXTERNAL_SOURCE_INSTALL') == 'all') || (Autoproj.user_config('CDFF_EXTERNAL_SOURCE_INSTALL') == 'required') then
      Autoproj.add_osdeps_overrides 'opencv', :package => 'cdff/external/opencv', :force => true
-     Autoproj.add_osdeps_overrides 'slam/pcl', :package => 'cdff/external/pcl', :force => true   
+     # slam/pcl: use system libpcl-dev (1.14.0 on Ubuntu 24.04); no longer need source build
 end
 
 asn_compiler = "#{ENV['AUTOPROJ_CURRENT_ROOT']}/tools/asn1scc/asn1.exe"
